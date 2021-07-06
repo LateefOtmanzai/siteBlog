603da367d6517db1fbe48378a9735f1fd9e96294
Lateef OTMANZAI <lateefotmanzai@gmail.com>
2021-07-06 09:12:58 +0200

    dev entité + structure HTML CSS + fixtures

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,11 +10,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class BlogController extends AbstractController
 {
     /**
+     * Méthode permettant d'afficher l'ensemble des articles du blog
+     * 
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function blog(): Response
     {
-        return $this->render('blog/index.html.twig', [
+        // traitements raquete selection BDD des articles
+
+        $repoArticles = $this->getDoctrine()->getRepository(Article::class);
+        dump($repoArticles);
+
+        $articles = $repoArticles->findAll();
+        dump($articles);
+
+        return $this->render('blog/blog.html.twig', [
             'controller_name' => 'BlogController',
         ]);
     }
@@ -27,5 +38,15 @@ class BlogController extends AbstractController
             'title' => 'Blog dédié à la musique, viendez voir, ça déchire !!!',
             'age' => 25
         ]);
+    }
+
+    /**
+    * Méthode permettent d'afficher la page d'accueil du blog Symfony
+     * @Route("/blog/12", name="blog_show")
+     */
+
+     public function show(): response
+     {
+        return $this->render('blog/show.html.twig');
     }
 }
